96274d036ef975fb666e8562f8c8d730f4a0a632
Yuvaraj Loganathan <uvaraj6@gmail.com>
2019-05-22 22:07:17 +0530

    Working Validator

--- src/main/java/io/grpc/examples/helloworld/GreeterImplImpl.java
+++ src/main/java/io/grpc/examples/helloworld/GreeterImplImpl.java
@@ -1,36 +1,42 @@
 package io.grpc.examples.helloworld;
 
-import io.envoyproxy.pgv.*;
+import io.envoyproxy.pgv.ReflectiveValidatorIndex;
+import io.envoyproxy.pgv.ValidationException;
+import io.envoyproxy.pgv.ValidatorImpl;
+import io.envoyproxy.pgv.ValidatorIndex;
 import io.grpc.stub.StreamObserver;
 
 public class GreeterImplImpl extends GreeterGrpc.GreeterImplBase {
+    private ValidatorIndex reflectiveValidatorIndex = new ReflectiveValidatorIndex();
+
     @Override
     public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
-        System.out.println("Helll!!" + req.getName() + "\n");
-        ValidatorIndex explicitValidatorIndex = new ExplicitValidatorIndex();
-        ValidatorIndex reflectiveValidatorIndex = new ReflectiveValidatorIndex();
-        Validator<HelloRequest> objectValidator = reflectiveValidatorIndex.validatorFor(HelloRequest.class);
-        System.out.println("xxxxxx Me " + objectValidator.isValid(req));
-
-//
-//
-//        Validator<HelloRequest> validator = validatorIndex.validatorFor(HelloRequest.class);
-//        boolean valid = validator.isValid(req);
-        HelloworldValidator.HelloRequestValidator helloRequestValidator = new HelloworldValidator.HelloRequestValidator();
+        String error = null;
+        System.out.println(req.getName());
+//        System.out.println("Helll!!" + req.getName() + "\n");
+//        ValidatorIndex explicitValidatorIndex = new ExplicitValidatorIndex();
+        ValidatorImpl validator = HelloworldValidator.validatorFor(HelloRequest.class);
         try {
-            helloRequestValidator.assertValid(req, reflectiveValidatorIndex);
+            validator.assertValid(req, reflectiveValidatorIndex);
         } catch (ValidationException e) {
-            e.printStackTrace();
+            error = e.getMessage();
+//            e.printStackTrace();
         }
-        HelloworldValidator.HelloRequestValidator requestValidator = new HelloworldValidator.HelloRequestValidator();
 
+//        HelloworldValidator.HelloRequestValidator helloRequestValidator = new HelloworldValidator.HelloRequestValidator();
 //        try {
-//            validator.isValid(req);
+//            helloRequestValidator.assertValid(req, reflectiveValidatorIndex);
 //        } catch (ValidationException e) {
 //            e.printStackTrace();
 //        }
-//        System.out.println("valid---" + valid);
-        HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
+//        HelloworldValidator.HelloRequestValidator requestValidator = new HelloworldValidator.HelloRequestValidator();
+        HelloReply reply;
+        if( error != null){
+            reply = HelloReply.newBuilder().setMessage("Error Occurred! " + error).build();
+        }else {
+            reply = HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
+        }
+
         responseObserver.onNext(reply);
         responseObserver.onCompleted();
     }
